0e980b33976e58402fda3165105d5e3feb8d3905
Milos Pavicic <milospavicic@yahoo.com>
2018-02-09 20:02:04 +0100

    Like/Dislike rating, and making a comment on a video page

--- js/videoPage.js
+++ js/videoPage.js
@@ -1,5 +1,32 @@
 $(document).ready(function(e) {
     loadComments();
+    
+    $('#likeButton').click(function() {
+        if($('#dislikeButton').prop('disabled')==true){
+            var likes ='<span class="glyphicon glyphicon-thumbs-up"></span> '+(parseInt($('#likeButton').text())+1);
+            var dislikes ='<span class="glyphicon glyphicon-thumbs-down"></span> '+(parseInt($('#dislikeButton').text())-1);
+            $('#dislikeButton').html(dislikes);
+            $(this).html(likes); 
+            $('#dislikeButton').prop('disabled',false);
+        }else{
+            var likes ='<span class="glyphicon glyphicon-thumbs-up"></span> '+(parseInt($('#likeButton').text())+1);
+            $(this).html(likes);
+        }
+        $(this).prop('disabled',true);
+    });
+    $('#dislikeButton').click(function() {
+        if($('#likeButton').prop('disabled')==true){
+            var likes ='<span class="glyphicon glyphicon-thumbs-up"></span> '+(parseInt($('#likeButton').text())-1);
+            var dislikes ='<span class="glyphicon glyphicon-thumbs-down"></span> '+(parseInt($('#dislikeButton').text())+1);
+            $(this).html(dislikes);
+            $('#likeButton').html(likes);
+            $('#likeButton').prop('disabled',false);
+        }else{
+            $(this).html(dislikes);
+            var dislikes ='<span class="glyphicon glyphicon-thumbs-down"></span> '+(parseInt($('#dislikeButton').text())+1);
+        }
+        $(this).prop('disabled',true);
+    });
 });
 function randomDate(start, end) {
     var newDate = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
@@ -15,23 +42,13 @@ function loadComments(){
 		dataType: 'json',
 		success: function(response) {
 
-			// Uzimamo samo prvih 50 slika
 			for(var i=0; i<maxComments; i++) {
 				var comment = response[i];
                 var newLikes = Math.floor(Math.random() * 999);
                 var newName = comment.name.substring(0, 7);
                 var newDate = 'Posted on: '+randomDate(new Date(2010, 0, 1), new Date());
-                var newDiv = $('<div class="col-md-12 col-sm-12"><div class="thumbnail" id="comment"><p id="commentOwner">'+newName+'</p><p id="commentDate">'+newDate+'</p><div class="commentText"><p>'+comment.body+'</p></div><button type="button" class="btn btn-default">Reply</button><p>'+newLikes+'</p><div class="btn-group" id="commLDBtnGroup"><button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-thumbs-up"></span></button><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-thumbs-down"></span> </button></div></div></div>')
-                /*
-				 	Da bi kasnije prikazali veliku verziju slike i njen naslov bez slanja zahteva na server, u data atribute 'largeImg' i 'title'
-					cemo smestiti ove vrednosti. Da atributi nam omogucavaju definisanje proizvoljnih atributa u HTML elementima i kreiraju se i
-					citaju uz pomoc 'data' funkcije (za razliku od standardnih HTML atributa za koje koristimo funkciju 'attr').
-				*/
-				//newDiv.data('largeImg', slika.url);
-				//newDiv.data('title', slika.title);
+                var newDiv = $('<div class="col-md-12 col-sm-12"><div class="thumbnail" id="comment"><p id="commentOwner">'+newName+'</p><p id="commentDate">'+newDate+'</p><div class="commentText"><p>'+comment.body+'</p></div><button type="button" class="btn btn-default">Reply</button><p id="likes">'+newLikes+'</p><div class="btn-group" id="commLDBtnGroup"><button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-thumbs-up"></span></button><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-thumbs-down"></span> </button></div></div></div>')
 
-				// Nakon sto smo postavili sve sto nam treba u vezi novokreiranog diva, dodajemo ga u postojecu HTML strukuru.
-				// Kokrentno, ovde nove divove dodajemo na kraj elementa sa id-em 'content'
 				$('.commentRow').append(newDiv);
 			}
 
@@ -44,3 +61,4 @@ function loadComments(){
 function refreshComments() {
     loadComments();
 }
+
